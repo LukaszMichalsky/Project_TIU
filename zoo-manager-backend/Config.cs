ab9f7ef594b21c13db36f2bd6479a55889db6670
Jakub Wyciślik <jakuwyc150@gmail.com>
2021-05-29 19:43:50 +0200

    Created zoo-manager-backend project

---
+++ zoo-manager-backend/Config.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zoo_manager_backend {
+    public abstract class Config {
+        public static readonly string DB_CONNECTION_STRING = "";
+    }
+}
---
+++ zoo-manager-backend/Services/AnimalsService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zoo_manager_backend.Services {
+    public class AnimalsService {
+    }
+}
