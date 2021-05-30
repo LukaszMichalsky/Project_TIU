69fb9c4f5d256d7bc02412309f07392da61773d0
Jakub Wyciślik <jakuwyc150@gmail.com>
2021-05-30 19:50:21 +0200

    Added model and service for 'Category' table

--- zoo-manager-backend/Controllers/CategoriesController.cs
+++ zoo-manager-backend/Controllers/CategoriesController.cs
@@ -1,13 +1,46 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using zoo_manager_backend.Models;
+using zoo_manager_backend.Services;
+
 namespace zoo_manager_backend.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class CategoriesController : ControllerBase {
+        private readonly MongoService<Category> mongoService;
+
+        public CategoriesController(MongoService<Category> mongoService) {
+            this.mongoService = mongoService;
+            mongoService.CollectionNamespace = "categories";
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public List<Category> GetCategories() {
+            return mongoService.Find((FilterDefinition<Category>) (category => true));
+        }
+
+        [HttpGet]
+        [Route("get/{id}")]
+        public Category GetCategory(int id) {
+            return mongoService.Find(new FilterDefinitionBuilder<Category>().Where(category => category.Id == id)).SingleOrDefault();
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public Category AddCategory([FromBody] Category newCategory) {
+            int availableIndex = mongoService.GetAvailableId();
+
+            return mongoService.InsertOne(new Category() {
+                CategoryName = newCategory.CategoryName,
+                Id = availableIndex
+            });
+        }
     }
 }
---
+++ zoo-manager-backend/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zoo_manager_backend.Models {
+    public class Category : MongoModel {
+        [BsonElement("category_name")]
+        public string CategoryName { get; set; }
+    }
+}
