0e13ef75b1b701fa68fed49a0a571eb3771b5241
LukaszMichalsky <43893107+LukaszMichalsky@users.noreply.github.com>
2021-06-10 09:22:39 +0200

    Add navbar, title and routing

--- zoo-manager-frontend/src/app/app-routing.module.ts
+++ zoo-manager-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnimaltypeComponent } from './animaltype/animaltype.component';
+import { CategoryComponent } from './category/category.component';
+import { FoodComponent } from './food/food.component';
+import { ZookeeperComponent } from './zookeeper/zookeeper.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "category", component: CategoryComponent},
+  {path: "zookeeper",component: ZookeeperComponent},
+  {path: "food",component: FoodComponent},
+  {path: "animaltype", component: AnimaltypeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- zoo-manager-frontend/src/app/app.component.ts
+++ zoo-manager-frontend/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: []
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'zoo-manager-frontend';
